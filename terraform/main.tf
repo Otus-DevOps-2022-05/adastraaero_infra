75a8f29aad3a26600b0b7f4712260fe916ee6869
Dmitry <artofsky@mail.ru>
2022-07-03 06:26:41 -0700

    tr1 try4

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,6 @@
-#terraform {
-#  required_providers {
-#    yandex = {
-#      source  = "yandex-cloud/yandex"
-#      version = "~> 0.35"
-#    }
-#  }
-  #  required_version = "~> 0.13"
-#}
 
 provider "yandex" {
+  version                  = 0.35
   service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
