24df9778dddcaa726b6272665fdee9f7093be912
Dmitry <artofsky@mail.ru>
2022-07-18 10:49:47 -0700

    ansible2

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-#echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-#apt-get install -y apt-transport-https ca-certificates
-sudo cp /tmp/mongodb.service /etc/systemd/system/
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+apt-get install -y apt-transport-https ca-certificates
 apt-get update
-chown 777 /etc/systemd/system/mongodb.service
-apt install -y mongodb
-systemctl start mongodb
-systemctl enable mongodb
+echo "Sleep 30 sec for apt update"; sleep 30s; echo "start apt install"
+apt-get install -y mongodb-org
+systemctl start mongod
+systemctl enable mongod
